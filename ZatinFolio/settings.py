5534a8ee0ea0d582332e6a1af39d410ced5b7289
zatin <yinmazatin21@gmail.com>
2021-04-14 18:59:33 +0700

    remove email sending

--- ZatinFolio/settings.py
+++ ZatinFolio/settings.py
@@ -169,11 +169,3 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
-
-# Email Configuration
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_USE_TLS = True
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = "zatinmail@gmail.com"
-# EMAIL_HOST_PASSWORD = "zatinmail9999911111@"
